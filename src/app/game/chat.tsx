c8e91bb75dfab27d009ecddef5f26f038485242d
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-01-13 23:40:45 +0100

    Emptying the chat text input when pressing the enter key

--- src/app/game/chat.tsx
+++ src/app/game/chat.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function Chat(){
+    const [inputValue, setInputValue] = useState('');
+
+    const handleKeyDown = (event: any) => {
+        if (event.key === 'Enter') {
+            setInputValue('');
+        }
+    };
+
     return (
         <div className="flex flex-col min-w-[200px] w-1/4 h-full">
             <div className="w-full h-[95vh] bg-gray-900 border-solid"></div>
 
             <div className="w-full h-[5vh] bg-gray-700 border-solid px-2">
-                <input className="w-full h-full bg-inherit outline-none" type="text" placeholder="Message chat"></input>
+                <input 
+                    type="text" 
+                    className="w-full h-full bg-inherit outline-none" 
+                    placeholder="Message chat"
+                    value={inputValue}
+                    onKeyDown={handleKeyDown}
+                    onChange={(event) => setInputValue(event.target.value)}
+                />
             </div>
         </div>
     );
