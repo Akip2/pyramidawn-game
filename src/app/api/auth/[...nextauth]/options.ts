e5e14d9f3575e2a056fad606a8deff3a9762a9e4
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-01-17 21:58:53 +0100

    Authentification form with Github

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,32 @@
+import type {NextAuthOptions} from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import CrdentialsProvider from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+
+        CrdentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: 'Username', type: 'text' },
+                password: { label: 'Password', type: 'password' },
+            },
+
+            async authorize(credentials) {
+                //Where we retrieve data of the user in the database
+                const user = {id: "1", name: "antoine", password: "pass"};
+
+                if(credentials?.username === user.name && credentials?.password === user.password) {
+                    return user;
+                }
+                else {
+                    return null;
+                }
+            }
+        })
+    ],
+};
