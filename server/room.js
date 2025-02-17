7ea98cf3a42bcca6b51ee8bae4b7ad71cd3b38d2
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-02-17 01:41:10 +0100

    Role action requests

--- server/room.js
+++ server/room.js
@@ -2,7 +2,8 @@ import Player from "./player.js";
 import {setTimeout} from "node:timers";
 
 const possibleColors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"];
-const defaultRoles = ["slave", "slave", "priest", "guard", "cursed"];
+const defaultRoles = ["priest", "golem", "cursed"];
+const phases = ["Golem", "Priest", "Temple", "Cursed", "Morning", "Vote", "Judge"]
 
 function createPhase(name, duration) {
     return {
@@ -22,6 +23,7 @@ export default class Room {
 
         this.started = false;
         this.phase = "Waiting";
+        this.phaseIndex = -1;
 
         this.timer = null;
     }
@@ -74,16 +76,21 @@ export default class Room {
     }
 
     distributeRoles() {
-        let remainingRoles = this.roles;
+        const remainingRoles = this.roles;
         let playerIndex = 0;
 
         while (playerIndex < this.players.length) {
-            let roleIndex = Math.floor(Math.random() * remainingRoles.length);
-            let role = remainingRoles.splice(roleIndex, 1)[0];
+            const roleIndex = Math.floor(Math.random() * remainingRoles.length);
+            const role = remainingRoles.splice(roleIndex, 1)[0];
+            const player = this.players[playerIndex];
 
-            this.send("role", role, this.players[playerIndex].id);
+            player.setRole(role);
+            this.send("role", role, player.id);
             playerIndex++;
         }
+
+        clearTimeout(this.timer);
+        this.timer = setTimeout(() => this.nextPhase(), 20000);
     }
 
     disconnectPlayer(playerId) {
@@ -100,6 +107,53 @@ export default class Room {
             this.remainingColors.push(player.color);
             this.players.splice(this.players.indexOf(player), 1);
             this.io.to(this.id).emit("player-leave", player.serialize());
+        }
+    }
+
+    nextPhase() {
+        this.phaseIndex++;
+        if (this.phaseIndex >= phases.length) {
+            this.phaseIndex = 0;
+        }
+
+        this.phase = phases[this.phaseIndex];
+
+        let time = 0;
+        let validPhase = true;
+
+        switch (this.phase) {
+            case "Golem":
+            case "Priest":
+                time = 30*1000;
+                const roleName = this.phase.toLowerCase();
+                validPhase = this.roleAction(roleName);
+                break;
+
+            case "Temple":
+                time = 30*1000;
+                break;
+        }
+
+        if (validPhase) {
+            this.send("phase-change", createPhase(this.phase, time));
+            clearTimeout(this.timer);
+            this.timer = setTimeout(() => this.nextPhase(), time + 5000);
+        } else {
+            this.nextPhase();
+        }
+    }
+
+    getPlayerByRole(role) {
+        return this.players.find(player => player.isRole(role));
+    }
+
+    roleAction(role) {
+        const concernedPlayer = this.getPlayerByRole(role);
+        if (concernedPlayer != null && concernedPlayer.isAlive) {
+            this.send(role + "-action", {}, concernedPlayer.id);
+            return true;
+        } else {
+            return false;
         }
     }
 
