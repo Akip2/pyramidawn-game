d8b41544bf6d09751d480e82d570d1a948a6f2e8
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-02-19 15:45:37 +0100

    Color on player avatar

--- src/components/player-avatar.tsx
+++ src/components/player-avatar.tsx
@@ -3,13 +3,13 @@
 import {useAction} from "@/context/action-provider";
 import {useState} from "react";
 import PlayerData from "@/data/player-data";
-import MySvg from "../../public/egyptian_0.svg";
+import Avatar from "../../public/egyptian_0.svg";
 
 export default function PlayerAvatar(player: PlayerData) {
     const [selected, setSelected] = useState(false);
     const {action, addPlayer, removePlayer} = useAction();
 
-    let classNames = "transition duration-200 filter "
+    let classNames = "";
     if(action) {
         classNames += "cursor-pointer ";
         if(selected) {
@@ -34,9 +34,7 @@ export default function PlayerAvatar(player: PlayerData) {
         <div className="flex flex-col items-center -translate-y-1/2" onClick={avatarClick}>
             <p className="px-2 py-1 ml-5 bg-black bg-opacity-75 rounded-xl mb-2">{player.name}</p>
 
-            <MySvg className={"w-1/2 "+classNames}/>
+            <Avatar className={"w-1/2 transition duration-200 filter "+classNames} style={{ fill: player.color }} />
         </div>
     )
 }
-
-//<div className="w-full h-full border-black border" style={{backgroundColor: player.color}}></div>
