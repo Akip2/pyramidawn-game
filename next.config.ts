bae993974c5dbb25293bea6dada8fb81de9dd8e3
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-02-19 15:03:37 +0100

    Player avatar svg added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import {webpack} from "next/dist/compiled/webpack/webpack";
 
 const nextConfig: NextConfig = {
   //reactStrictMode: false
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        });
+
+        return config;
+    }
 };
 
 export default nextConfig;
--- src/components/player-avatar.tsx
+++ src/components/player-avatar.tsx
@@ -3,12 +3,13 @@
 import {useAction} from "@/context/action-provider";
 import {useState} from "react";
 import PlayerData from "@/data/player-data";
+import MySvg from "../../public/egyptian_0.svg";
 
 export default function PlayerAvatar(player: PlayerData) {
     const [selected, setSelected] = useState(false);
     const {action, addPlayer, removePlayer} = useAction();
 
-    let classNames = "w-[5vw] h-[15vw] flex flex-col items-center -translate-y-1/2 transition duration-200 filter "
+    let classNames = "transition duration-200 filter "
     if(action) {
         classNames += "cursor-pointer ";
         if(selected) {
@@ -30,10 +31,12 @@ export default function PlayerAvatar(player: PlayerData) {
     }
 
     return (
-        <div className={classNames} onClick={avatarClick}>
-            <p className="px-2 py-1 bg-black bg-opacity-75 rounded-xl mb-2">{player.name}</p>
+        <div className="flex flex-col items-center -translate-y-1/2" onClick={avatarClick}>
+            <p className="px-2 py-1 ml-5 bg-black bg-opacity-75 rounded-xl mb-2">{player.name}</p>
 
-            <div className="w-full h-full border-black border" style={{backgroundColor: player.color}}></div>
+            <MySvg className={"w-1/2 "+classNames}/>
         </div>
     )
 }
+
+//<div className="w-full h-full border-black border" style={{backgroundColor: player.color}}></div>
