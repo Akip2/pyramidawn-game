4b520d0bb5771d833063fce32acb5730dacc39c3
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-04-09 23:21:41 +0200

    Death messages are colored red

--- src/data/message/death-message.tsx
+++ src/data/message/death-message.tsx
@@ -5,15 +5,21 @@ import PlayerData from "@/data/player-data";
 export default class DeathMessage implements IMessage {
     type: string = "death";
     content: string;
+    roleText: string;
 
     constructor(v: PlayerData, reason: string, role: string) {
-        this.content = `${v.name} was ${reason}.\nHe was ${role}.`;
+        this.content = `${v.name} was ${reason}.`;
+        this.roleText = `Their role was ${role}`;
     }
 
     getHTML(key: string): React.JSX.Element {
         return (
-            <div key={key} className="bg-blue-900 p-4 text-white text-center break-words rounded-xl text-lg">
-                <p>{this.content}</p>
+            <div key={key} className="bg-red-900 p-4 text-white text-center break-words rounded-xl text-lg">
+                <p>
+                    {this.content}
+                    <br/>
+                    {this.roleText}
+                </p>
             </div>
         );
     }
