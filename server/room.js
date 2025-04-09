f7fc200efa6f92ec3bedbcc6d8ca2370ccc74efe
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-04-09 15:13:34 +0200

    Morning phase

--- server/room.js
+++ server/room.js
@@ -10,7 +10,7 @@ import RolePhase from "./phases/non-cyclic/role-phase.js";
 import PlayerManager from "./player-manager.js";
 import RequestSender from "./request-sender.js";
 
-const defaultRoles = ["priest", "wraith","slave"];
+const defaultRoles = ["priest", "wraith", "golem", "slave", "slave"];
 
 export default class Room {
     constructor(io, id, roles = [...defaultRoles]) {
