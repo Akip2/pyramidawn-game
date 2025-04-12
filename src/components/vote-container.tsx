ff5a517bef006ba7d58a266802743bef40f9355c
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-04-12 23:41:26 +0200

    Animation for vote bubbles

--- src/components/vote-container.tsx
+++ src/components/vote-container.tsx
@@ -1,14 +1,26 @@
 'use client'
 
 import PlayerData from "@/data/player-data";
+import {motion} from "framer-motion";
 
 export default function VoteContainer(props: { voters: PlayerData[] }) {
     const voters = props.voters;
     return (
-        <div className="flex flex-row items-center justify-around w-full h-8">
+        <motion.div
+            className="flex flex-row items-center justify-around w-full h-8"
+            animate={{ y: [0, -5, 0] }}
+            transition={{
+                duration: 2,
+                repeat: Infinity,
+                repeatType: "loop",
+                ease: "easeInOut"
+            }}
+        >
             {voters.map((voter, index) => (
-                <div key={index} className="rounded-2xl w-8 h-full border-white border-4" style={{backgroundColor: voter.color}}></div>
+                <div key={index} className="rounded-2xl w-8 h-full mr-1 ml-1 border-white border-4"
+                     style={{backgroundColor: voter.color}}>
+                </div>
             ))}
-        </div>
+        </motion.div>
     )
 }
