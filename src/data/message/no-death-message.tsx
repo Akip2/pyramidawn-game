9e3233ccfe503e5a3e37a8559913c7dd98d9577f
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-04-13 00:21:01 +0200

    Fixing eslint warnings

--- src/data/message/no-death-message.tsx
+++ src/data/message/no-death-message.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import IMessage from "./imessage";
-import PlayerData from "@/data/player-data";
 import DefaultMessage from "@/data/message/default-message";
 
 export default class NoDeathMessage extends DefaultMessage {
